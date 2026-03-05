5e36d30cc0e75d70b110c01566b424cef99b9606
kirusha-13 <kirillukashevich2007@gmail.com>
2026-03-06 00:22:45 +0300

    Zadanie 2/3 complete

--- main.cpp
+++ main.cpp
@@ -32,6 +32,74 @@ void dlfake(BiList< T >* fake) noexcept
   ::operator delete(fake);
 }
 
+template< class T >
+BiList< T >* insert(BiList< T >* prev, const T& data)
+{
+  BiList< T >* el = new BiList< T >;
+  el->val = data;
+  el->prev = prev;
+  el->next = prev->next;
+  el->prev->next = el;
+  el->next->prev = el;
+  return el;
+}
+
+template< class T >
+BiList< T >* add(BiList< T >* next, const T& data)
+{
+  BiList< T >* el = new BiList< T >;
+  el->val = data;
+  el->next = next;
+  el->prev = next->prev;
+  el->prev->next = el;
+  el->next->prev = el;
+  return el;
+}
+
+template< class T >
+BiList< T >* makeBiList()
+{
+  BiList< T >* el = new BiList< T >;
+  BiList< T >* rt = fakeStart(el);
+  fakeEnd(el);
+  el->prev->next = el->next;
+  el->next->prev = el->prev;
+  delete el;
+  return rt;
+}
+
+template< class T >
+BiList< T >* makeBiList(const T& data)
+{
+  BiList< T >*el = new BiList< T >;
+  el->val = data;
+  fakeStart(el);
+  fakeEnd(el);
+  return el;
+}
+
+template< class T >
+BiList< T >* cut(BiList< T >* el)
+{
+  BiList< T >* r = el->next;
+  el->next = el->next->next;
+  el->next->prev = el;
+  delete r;
+  return el;
+}
+
+template< class T >
+void clear(BiList< T >* fakeStart, BiList< T >* fakeEnd)
+{
+  while(fakeStart->next != fakeEnd)
+  {
+    cut(fakeStart);
+  }
+
+  dlfake(fakeStart);
+  dlfake(fakeEnd);
+}
+
 int main ()
 {
   return 0;
