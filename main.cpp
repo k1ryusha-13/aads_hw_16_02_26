52fb7360fc9f220c36be8c744652309bfa486b20
kirusha-13 <kirillukashevich2007@gmail.com>
2026-03-06 00:18:49 +0300

    Number 0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+template< class T >
+struct BiList
+{
+  T val;
+  BiList< T >* next;
+  BiList< T >* prev;
+};
+
+template< class T >
+BiList< T >* fakeStart(BiList< T >* next)
+{
+  BiList< T >* el = static_cast< BiList< T >* >(::operator new(sizeof(BiList< T >)));
+  el->next = next;
+  next->prev = el;
+  return el;
+}
+
+template< class T >
+BiList< T >* fakeEnd(BiList< T >* prev)
+{
+  BiList< T >* el = static_cast< BiList< T >* >(::operator new(sizeof(BiList< T >)));
+  el->prev = prev;
+  prev->next = el;
+  return el;
+}
+
+template< class T >
+void dlfake(BiList< T >* fake) noexcept
+{
+  ::operator delete(fake);
+}
+
 int main ()
 {
   return 0;
